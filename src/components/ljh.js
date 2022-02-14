2013eea0b9f15f4abeb2477883e30f55251f16d2
boho472 <juno7014@naver.com>
2022-02-14 14:37:59 +0900

    first commitment

---
+++ src/components/ljh.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ljh() {
+  return (
+    <div
+      className="contents"
+      style={{ height: "100vh", paddingTop: "50vh", backgroundColor: "black" }}
+    >
+      안녕하세요.🐘
+    </div>
+  );
+}
+
+export default ljh;
